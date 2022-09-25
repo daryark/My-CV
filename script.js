4327140217119943c63bba3695cd1610f8774945
Dasha <yarkovska11@gmil.com>
2022-09-25 22:23:03 +0300

    add change mode function

--- script.js
+++ script.js
@@ -4,12 +4,3 @@ dayNight.addEventListener("click", darkTheme);
 function darkTheme() {
     document.body.classList.toggle("dark-theme");
 }
-
-
-let uaEn = document.getElementById("ua-en");
-uaEn.addEventListener("click", changeLang);
-
-function changeLang() {
-    let i = alert("i");
-    document.body.classList.toggle("ua-lang");
-}
